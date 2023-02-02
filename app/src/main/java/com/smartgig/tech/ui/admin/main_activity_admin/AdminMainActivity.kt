c1849e4c5d9fdf8beea2cf68e3c3c8ed1d4c324e
Nownitya Sharma <nownitya@gmail.com>
2023-02-02 10:59:02 +0530

    Initial Push

--- app/src/main/java/com/smartgig/tech/ui/admin/main_activity_admin/AdminMainActivity.kt
+++ app/src/main/java/com/smartgig/tech/ui/admin/main_activity_admin/AdminMainActivity.kt
@@ -1,15 +1,25 @@
 package com.smartgig.tech.ui.admin.main_activity_admin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 import com.smartgig.tech.R
 import com.smartgig.tech.databinding.ActivityAdminMainBinding
+import com.smartgig.tech.databinding.ActivityEmployeeMainBinding
 import com.smartgig.tech.ui.admin.home_page.AdminHomeFragment
+import com.smartgig.tech.ui.admin.leave_request.AdminLeaveRequestFragment
+import com.smartgig.tech.ui.login_activity.LoginActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,7 +29,6 @@ class AdminMainActivity : AppCompatActivity() {
     private lateinit var navigationView: NavigationView
     private lateinit var navController: NavController
     private lateinit var appConfig: AppBarConfiguration
-    private lateinit var toogle:ActionBarDrawerToggle
 
 
 
@@ -30,45 +39,55 @@ class AdminMainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        setUpView()
+        //  Toolbar
+        val toolbar = binding.toolbarAdmin
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
 
-    }
 
 
-    private fun setUpView() {
-        setSupportActionBar(binding.toolbarAdmin)
-        toogle = ActionBarDrawerToggle(
-            this,
-            binding.drawerLayoutAdmin,
-            R.string.drawer_Open,
-            R.string.drawer_Close
+        // NavigationView And Navigation Controller
+        navigationView = binding.navigationViewAdmin
+        navController = Navigation.findNavController(this, R.id.fragment_admin)
 
+        //  AppBarConfiguration
+
+        val drawerLayout = binding.drawerLayoutAdmin
+        appConfig = AppBarConfiguration(
+            navController.graph,drawerLayout
         )
-        binding.drawerLayoutAdmin.addDrawerListener(toogle)
-        toogle.syncState()
+//        val drawerLayout = binding.drawerLayoutAdmin
+//        appConfig = AppBarConfiguration(
+//            setOf(
+//                R.id.adminHomeFragments,
+//                R.id.adminLeaveRequestFragment
+//
+//            ),drawerLayout
+//        )
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        navigationView.setNavigationItemSelectedListener {
 
-            when (it.itemId) {
-                R.id.adminHomeFragments ->{
-//                    changeFragment(AdminHomeFragment::class.java)
-                }
-            }
-            true
 
-        }
-
+        NavigationUI.setupWithNavController(binding.toolbarAdmin, navController, appConfig)
+        navigationView.setupWithNavController(navController)
     }
-
-    private fun changeFragment(fragment: Class<AdminHomeFragment>) {
-
-        TODO("Not yet implemented")
-    }
-
 
     override fun onSupportNavigateUp(): Boolean {
 //        return super.onSupportNavigateUp()
         return NavigationUI.navigateUp(navController,binding.drawerLayoutAdmin)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_logout -> {
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
